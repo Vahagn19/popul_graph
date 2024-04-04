7fbb206af56398915201d32598b42b73ccc42ef6
Vahag <gevorgyanvahagn74@gmail.com>
2024-04-04 18:22:27 +0400

    sidebar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Graph from './components/Graph';
 import Tester from './components/Tester';
 import SideBar from './components/SIdebar';
-
+import Navbar from './components/Navbar/ui/Navbar';
 function App() {
   return (
     <div className="App">
       {/* <Graph/> */}
  
       {/* <Tester/> */}
-      <SideBar/>
+      {/* <SideBar/> */}
+      <Navbar/>
     </div>
   );
 }
